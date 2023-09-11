11141854fab1fd3ee5b1eb3f86be8a0096bd0313
David Dume <dumedavid11@gmail.com>
2023-09-11 21:55:58 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <MainLayout>
+        <div class="mt-4 max-w-[1200px] mx-auto px-2" id="IndexPage">
+            Index Page
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+
+</script>
