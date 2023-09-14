d537386c0fa18d35643b77aff8e0d23b13f68bcc
David Dume <dumedavid11@gmail.com>
2023-09-14 18:35:21 +0200

    get items from db

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -17,9 +17,9 @@
                     </div>
                 </div>
                 <div class="md:w-[60%] bg-white p-3 rounded-lg">
-                    <div v-if="true">
-                        <p class="mb-2">Title </p>
-                        <p class="font-light text-[12px] mb-2">Description Section</p>
+                    <div v-if="product && product.data">
+                        <p class="mb-2">{{ product.data.title }} </p>
+                        <p class="font-light text-[12px] mb-2">{{ product.data.description }}</p>
                     </div>
                     <div class="flex items-center pt-1.5">
                         <span class="h-4 min-2-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
@@ -68,16 +68,23 @@ const userStore = useUserStore();
 
 const route = useRoute()
 
+let product = ref(null)
 let currentImage = ref(null)
 
-
-onMounted(() => {
-    watchEffect(() => {
-        currentImage.value = 'https://picsum.photos/id/82/800/800'
-        images.value[0] = 'https://picsum.photos/id/87/800/800'
-
-    })
+onBeforeMount(async () => {
+    product.value = await useFetch(`/api/prisma/get-product-by-id/${route.params.id}`)
 })
+
+
+watchEffect(() => {
+    if(product.value && product.value.data) {
+        currentImage.value = product.value.data.url
+        images.value[0] = product.value.data.url
+        userStore.isLoading = false
+    }
+
+})
+
 
 const isInCart = computed(() => {
     let res = false
@@ -90,7 +97,11 @@ const isInCart = computed(() => {
 })
 
 const priceComputed = computed(() => {
-    return '26.40'
+    if(product.value && product.value.data) {
+       return product.value.data.price / 100
+    }
+
+    return '0.00'
 })
 
 let images = ref([
@@ -103,7 +114,7 @@ let images = ref([
 ])
 
 const addToCart = () => {
-    alert('ADDED')
+    userStore.cart.push(product.value.data)
 }
 
 </script>
